a838afd3bd9e87644cfd7cb907bd3d7c7dd29c95
Rodrigo Hernando <rhernandog@gmail.com>
2020-05-19 13:01:29 -0400

    Relational Data Update - Added relations between the different data types - Added the comments dummy data - Created method to update the dummy data adding relations between each type - Added comments schema and query definition

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import { GraphQLServer } from "graphql-yoga";
-import users from "./finalUsers.json";
-import posts from "./finalPosts.json";
+import gql from "graphql-tag";
+import usersData from "./finalUsers.json";
+import postsData from "./finalPosts.json";
+import commentsData from "./finalComments.json";
+
+const { usersData: users } = usersData;
+const { postsData: posts } = postsData;
+const { commentsData: comments } = commentsData;
 
 // Type definitions (schema)
-const typeDefs = `
+const typeDefs = gql`
   type Query {
     user: User!
     users(query: String): [User!]!
     posts(query: String): [Post!]!
+    comments: [Comment!]!
   }
 
   type Post {
@@ -16,6 +23,7 @@ const typeDefs = `
     body: String!
     published: String!
     author: User!
+    comments: [Comment!]!
   }
 
   type User {
@@ -24,6 +32,15 @@ const typeDefs = `
     email: String!
     age: Int
     posts: [Post!]!
+    comments: [Comment!]!
+  }
+
+  type Comment {
+    id: ID!
+    title: String!
+    body: String!
+    user: User!
+    post: Post!
   }
 `
 
@@ -32,33 +49,50 @@ const resolvers = {
   Query: {
     users(parent, args, ctx, info) {
       const filteredUsers = args.query ?
-        users.usersData.filter(
+        users.filter(
           user => user.name.match(new RegExp(args.query, "gi")) != null
         ) :
-        users.usersData;
+        users;
       return filteredUsers;
     },
     posts(parent, args, ctx, info) {
       const _posts = args.query ?
-        posts.postsData.filter(
+        posts.filter(
           post => post.title.match(new RegExp(args.query, "gi")) != null
         ) :
-        posts.postsData;
+        posts;
       return _posts;
     },
+    comments(parent, args, ctx, info) {
+      return comments;
+    }
   },
   Post: {
     author(parent, args, ctx, info) {
-      const postAuthor = users.usersData.find(user => user.id == parent.author);
+      const postAuthor = users.find(user => user.id == parent.author);
       return postAuthor;
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => comment.post === parent.id);
     }
   },
   User: {
     posts(parent, args, ctx, info) {
-      const userPosts = posts.postsData.filter(post => parent.posts.indexOf(post.id) > -1);
+      const userPosts = posts.filter(post => post.author === parent.id);
       return userPosts;
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter(comment => comment.user === parent.id);
     }
   },
+  Comment: {
+    user(parent, args, ctx, info) {
+      return users.find(user => user.id === parent.user);
+    },
+    post(parent, args, ctx, info) {
+      return posts.find(post => post.id === parent.post);
+    }
+  }
 };
 
 const server = new GraphQLServer({
--- src/processPosts.js
+++ src/processPosts.js
@@ -2,31 +2,45 @@ const fs = require("fs");
 const process = require("process");
 
 const _users = fs.readFileSync("./users.json");
-const users = JSON.parse(_users).users;
+const users = JSON.parse(_users).usersData;
+const _comments = fs.readFileSync("./finalComments.json");
+const comments = JSON.parse(_comments).commentsData;
 const _posts = fs.readFileSync("./posts.json");
 const posts = JSON.parse(_posts).postsData;
 
-const completePosts = posts.map(post => {
-  const randomUser = users[Math.round(Math.random() * users.length)];
-  const newPost = { ...post, author: randomUser.id };
-  users.forEach(user => {
-    if (user.id == randomUser.id) {
-      if (user.posts) {
-        user.posts.push(post.id);
-      } else {
-        user.posts = [post.id];
-      }
-    }
+const updatedPosts = posts.map(post => {
+  const randomUser = users[Math.floor(Math.random() * users.length)];
+  const postComments = [];
+  comments.forEach(comment => {
+    if (comment.post === post.id) postComments.push(comment.id);
   });
+  const newPost = {
+    ...post,
+    author: randomUser.id,
+    comments: postComments
+  };
   return newPost;
 });
 
+const updatedUsers = users.map(user => {
+  const newUser = { ...user, posts: [], comments: [] };
+  updatedPosts.forEach(post => {
+    if (post.author === user.id) {
+      newUser.posts.push(post.id);
+    }
+  });
+  comments.forEach(comment => {
+    if (comment.user === user.id) newUser.comments.push(comment.id);
+  });
+  return newUser;
+});
+
 const finalPosts = {
-  "postsData": completePosts
+  "postsData": updatedPosts
 };
 
 const finalUsers = {
-  "usersData": users
+  "usersData": updatedUsers
 };
 
 fs.writeFileSync("./finalPosts.json", JSON.stringify(finalPosts, null, 2));
